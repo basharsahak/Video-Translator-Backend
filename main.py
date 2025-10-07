2b24a806e5455fcca708f7c4ffeb8f8457ed6ba9
basharsahak <basharsahak2022@gmail.com>
2025-10-07 05:44:30 -0700

    Add FastAPI app instance

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-# main.py
-print("Hello Video Translator Backend!")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello Video Translator Backend!"}
+
